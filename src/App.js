4beced89558c06236eb26ea8f83a128a5f43591f
ramshadKen4 <ramshadken@icloud.com>
2023-03-30 01:25:16 +0530

    set router for home ,signup and login

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Home from './Components/Home';
+import Signup from './Components/Signup';
+import Login from './Components/Login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <BrowserRouter>
+            <Routes>
+              <Route exact path="/" element={<Home/>}/> 
+              <Route path="/signup" element={<Signup />}/>
+              <Route path="/login" element={<Login/>}/>
+            </Routes>
+          </BrowserRouter>
+        </div>
       </header>
     </div>
   );
